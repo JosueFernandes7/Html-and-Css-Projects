5aac2c9c149ef7990327b9a68f85389fe67791ea
Josue Fernandes <josue7mf@hotmail.com>
2022-06-27 23:56:01 -0300

    menu bar

---
+++ Personal Projects/Cadastro para Escola/menu.js
@@ -0,0 +1,9 @@
+const menu = document.getElementById('menu');
+const ul = document.querySelector('.menu-info');
+function activeMobile() {
+  ul.classList.toggle('active');
+  menu.classList.toggle('active');
+  
+}
+
+menu.addEventListener('click',activeMobile)
